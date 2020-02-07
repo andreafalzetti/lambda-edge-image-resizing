e0fc45bfafd956525d05eaf9a327316af9b89bd3
Ed Bentinck <edbentinck@gmail.com>
2020-02-07 16:52:52 -0600

    Downgrade node JS version to one supported by AWS Lambda

--- docker_build.sh
+++ docker_build.sh
@@ -3,7 +3,7 @@
 docker build --tag amazonlinux:nodejs .
 
 # Install the sharp and querystring module dependencies and compile the 'Origin-Response' function
-docker run --rm --volume ${PWD}/lambda/origin-response-function:/build amazonlinux:nodejs /bin/bash -c "source ~/.bashrc; npm init -f -y; npm install --arch=x64 --platform=linux --target=12.13.1 sharp --save; npm install querystring --save; npm install --only=prod"
+docker run --rm --volume ${PWD}/lambda/origin-response-function:/build amazonlinux:nodejs /bin/bash -c "source ~/.bashrc; npm init -f -y; npm install --arch=x64 --platform=linux --target=10.19.0 sharp --save; npm install querystring --save; npm install --only=prod"
 
 # Install the querystring module dependencies and compile the 'Viewer-Request' function
 docker run --rm --volume ${PWD}/lambda/viewer-request-function:/build amazonlinux:nodejs /bin/bash -c "source ~/.bashrc; npm init -f -y; npm install querystring --save; npm install --only=prod"
